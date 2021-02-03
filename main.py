d27595e93ca6e14ef125ee475bcfd956fcccbca9
Chemllei Joseph <chemllei0304@gmail.com>
2021-02-03 18:40:31 -0500

    dotenv implem'd for env vars

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from modules.db_ctrla import DB
 from modules.objects import Project
 
 if __name__ == "__main__":
-    Project("name", "descrip", "02/02/21", ["tool1", "tool2"]).to_string()
+    DB()
+    Project("name", "descrip", "02/02/21", ["tool1", "tool2"])
--- modules/db_ctrla.py
+++ modules/db_ctrla.py
@@ -1,23 +1,28 @@
+import os
+
 import MySQLdb
+from dotenv import load_dotenv
 
 
 class DB:
-    """
-CREATE TABLE IF NOT EXISTS projects(
-project_id INT AUTO-INCREMENT,
-name,
-descrip,
-start_date,
-tools_used,
-status,
-PRIMARY KEY (project_id));
-    """
     def __init__(self):
-        pass
+        load_dotenv()
+        create_table_stmt = "CREATE TABLE IF NOT EXISTS projects(" \
+                            "project_id INT AUTO_INCREMENT," \
+                            "name TEXT NOT NULL," \
+                            "descrip TEXT NOT NULL," \
+                            "start_date TEXT NOT NULL," \
+                            "tools_used TEXT NOT NULL," \
+                            "status TEXT NOT NULL," \
+                            "PRIMARY KEY (project_id));"
+        self.db_write(create_table_stmt)
 
     @staticmethod
     def db_read(stmt: str) -> list:
-        db = MySQLdb.connect("", "", "", "")
+        db = MySQLdb.connect(os.getenv("host"),
+                             os.getenv("user"),
+                             os.getenv("passwd"),
+                             os.getenv("db"))
         cursor = db.cursor()
         try:
             cursor.execute(stmt)
@@ -27,7 +32,10 @@ PRIMARY KEY (project_id));
 
     @staticmethod
     def db_write(stmt: str):
-        db = MySQLdb.connect("", "", "", "")
+        db = MySQLdb.connect(os.getenv("host"),
+                             os.getenv("user"),
+                             os.getenv("passwd"),
+                             os.getenv("db"))
         cursor = db.cursor()
         try:
             cursor.execute(stmt)
@@ -36,7 +44,7 @@ PRIMARY KEY (project_id));
             print(e)
 
     def create(self, new_project):
-        stmt = "INSERT INTO projects() VALUES ()" % new_project
+        stmt = "INSERT INTO projects (name, descrip, start_date, tools_used, status) VALUES ()" % new_project
         self.db_write(stmt)
         print("Added.")
 
