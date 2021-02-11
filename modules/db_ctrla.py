d078586b397aa35f3e1e5ee75d99ed7fb6cb3700
Chemllei Joseph <chemllei0304@gmail.com>
2021-02-10 23:54:54 -0500

    docstrings

--- modules/db_ctrla.py
+++ modules/db_ctrla.py
@@ -23,6 +23,7 @@ class DB:
 
     @staticmethod
     def db_read(stmt: str) -> list:
+        """Generic MySQL read query"""
         db = MySQLdb.connect(os.getenv("host"),
                              os.getenv("user"),
                              os.getenv("passwd"),
@@ -36,6 +37,7 @@ class DB:
 
     @staticmethod
     def db_write(stmt: str):
+        """Generic MySQL write query"""
         db = MySQLdb.connect(os.getenv("host"),
                              os.getenv("user"),
                              os.getenv("passwd"),
@@ -50,12 +52,14 @@ class DB:
         db.close()
 
     def add_project(self, new_project: Project):
+        """Add project to DB"""
         stmt = "INSERT INTO projects (name, descrip, start_date, tools_used, status) VALUES ('%s','%s','%s','%s','%s')" % (
             new_project.name, new_project.descrip, new_project.start_date, new_project.tools_used, new_project.status)
         self.db_write(stmt)
         print("Added.")
 
     def get_all_projects(self) -> List[Project]:
+        """Get all projects in DB"""
         results = []
         stmt = "SELECT * FROM projects"
         for row in self.db_read(stmt):
@@ -63,22 +67,26 @@ class DB:
 
         return results
 
-    def edit_project(self, project_id: int):
-        stmt = "UPDATE projects SET - = - WHERE project_id = '%d'" % project_id
-        self.db_write(stmt)
-        print("Updated.")
+    # def edit_project(self, project_id: int):
+    #     """Edit project"""
+    #     stmt = "UPDATE projects SET - = - WHERE project_id = '%d'" % project_id
+    #     self.db_write(stmt)
+    #     print("Updated.")
 
     def delete_project(self, project_id: int):
+        """Delete project from DB"""
         stmt = "DELETE FROM projects WHERE project_id = '%d'" % project_id
         self.db_write(stmt)
         print("Deleted.")
 
     def delete_all_projects(self):
+        """Delete all projects from DB"""
         stmt = "TRUNCATE table projects"
         self.db_write(stmt)
         print("All deleted.")
 
     def import_projects(self):
+        """Import multiple projects from CSV file into DB"""
         imported = []
         with open("input.csv") as file:
             csv_data = csv.reader(file)
@@ -90,11 +98,12 @@ class DB:
         for item in imported:
             item.to_string()
 
-        answer = input("Add these quotes? ")
+        answer = input("Add these projects? ")
         if answer == "Y" or answer == "y":
             for item in imported:
                 self.add_project(item)
 
     def get_project_by_id(self, project_id: int) -> list:
+        """Find project w/ specified id"""
         stmt = "SELECT * FROM projects WHERE project_id = '%d'" % project_id
         return self.db_read(stmt)
