66e8de94342216aeed1264358bd57c8387bfaa4d
Chemllei Joseph <chemllei0304@gmail.com>
2021-02-16 22:49:51 -0500

    starting work on prompts

--- modules/view.py
+++ modules/view.py
@@ -1,20 +1,21 @@
-from PyInquirer import prompt, print_json
+from PyInquirer import prompt
 
 
 class CmdLnInterface:
     def __init__(self):
-        questions = [
-            {
-                'type': 'list',
-                'name': 'theme',
-                'message': 'What do you want to do?',
-                'choices': [
-                    "Add Project",
-                    "Delete Project",
-                    "Delete All Projects",
-                    "Import Projects"
-                ]
-            }
-        ]
-        answers = prompt(questions)
-        print_json(answers)
+        main_options = [{
+            'type': 'list',
+            'name': 'main_options',
+            'message': 'What do you want to do?',
+            'choices': [
+                "Add Project",
+                "Delete Project",
+                "Delete All Projects",
+                "Import Projects"]}]
+
+        # add_questions = [{
+        #     'type': 'input',
+        #     'name': 'project_name',
+        #     'message': 'Name?'}]
+
+        _ = prompt(main_options)
