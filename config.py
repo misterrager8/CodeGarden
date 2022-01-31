d149016c1941d93b76bdcfa9bcdeb25fee316abd
Chemllei Joseph <chemllei0304@gmail.com>
2022-01-31 02:05:08 -0500

    Change config

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ import dotenv
 
 dotenv.load_dotenv()
 
-DEBUG = True
-ENV = "development"
+DEBUG = os.getenv("debug")
+ENV = os.getenv("flask_env")
+SECRET_KEY = os.getenv("secret_key")
 SQLALCHEMY_DATABASE_URI = os.getenv("db_url")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 60}
