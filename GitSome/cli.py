6ea6512bc90dc9a0a2ced29b2e9e3efffc11d40c
Chemllei Joseph <chemllei0304@gmail.com>
2022-08-10 18:55:47 -0400

    Changes (10Aug2022)
    - Change theme, style tweaks
    
    - Redo git tab layout
    
    - Add CLI version
    
    - Add PORT config variable

--- GitSome/cli.py
+++ GitSome/cli.py
@@ -1,7 +1,7 @@
 import click
-from GitSome import create_app, config
-from GitSome.models import Project
-import os
+from GitSome import create_app, config, db
+from GitSome.models import Project, Todo
+from pathlib import Path
 
 app = create_app(config)
 
@@ -16,21 +16,74 @@ def list_all():
     with app.app_context():
         for i in Project.query.all():
             click.secho(
-                "%s - %s TODO(S)"
-                % (i.name, i.get_todos(filter_='status = "Todo"').count()),
-                fg="green",
+                "[%s] %s - %s TODO(S)"
+                % (i.id, i.name, i.get_todos(filter_='status = "Todo"').count()),
+                fg="cyan",
             )
 
 
 @cli.command()
-@click.argument("name")
-def make_new(name):
-    os.mkdir(name)
-    with open(f"{name}/README.md", "w") as f:
-        f.write(f"# {name}\n---\n")
-    click.secho(f"Folder {name} created.", fg="green")
+@click.argument("id")
+def list_todos(id):
+    with app.app_context():
+        project_ = Project.query.get(int(id))
+        for i in project_.get_todos(filter_='status = "Todo"'):
+            click.secho("[%s] %s" % (i.id, i.task), fg="cyan")
+
+
+@cli.command()
+@click.argument("fullpath")
+def make_new(fullpath):
+    path_ = Path(fullpath)
+
+    path_.mkdir()
+    with app.app_context():
+        project_ = Project(name=path_.name, filepath=fullpath, pinned=True)
+        db.session.add(project_)
+        db.session.commit()
+
+        project_.git_command("git init")
+
+    with open(path_ / "README.md", "w") as f:
+        f.write(f"# {path_.name}\n---\n")
+
+    click.secho(f"Folder {path_.name} created.", fg="cyan")
+
+
+@cli.command()
+@click.argument("id")
+@click.argument("task")
+def add_todo(id, task):
+    with app.app_context():
+        project_ = Project.query.get(int(id))
+        todo_ = Todo(task=task, project_id=project_.id)
+        db.session.add(todo_)
+        db.session.commit()
+
+    click.secho("Todo added.", fg="cyan")
+
+
+@cli.command()
+@click.argument("id")
+def commit_todo(id):
+    with app.app_context():
+        todo_ = Todo.query.get(int(id))
+        todo_.commit()
+
+    click.secho("Todo committed.", fg="cyan")
+
+
+@cli.command()
+@click.argument("id")
+def delete_project(id):
+    with app.app_context():
+        project_ = Project.query.get(int(id))
+        db.session.delete(project_)
+        db.session.commit()
+
+        click.secho("Deleted.", fg="cyan")
 
 
 @cli.command()
 def web():
-    app.run()
+    app.run(port=app.config["PORT"])
--- GitSome/config.py
+++ GitSome/config.py
@@ -5,5 +5,6 @@ dotenv.load_dotenv()
 
 ENV = os.getenv("env")
 DEBUG = os.getenv("debug")
+PORT = os.getenv("port")
 SQLALCHEMY_DATABASE_URI = os.getenv("db_url")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
