a435a9627b81f78a67ab7029d8006441f0077356
misterrager8 <chemllei0304@gmail.com>
2022-09-22 14:56:23 -0400

    1

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="CodeGarden",
-    version="2.0.1",
-    long_description=open("README.md").read(),
-    license=open("LICENSE.md").read(),
-    entry_points={"console_scripts": ["garden=code_garden.cli:cli"]},
-)
