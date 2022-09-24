f56115d17caf12a9cad2306bfa936e08d55c8630
misterrager8 <chemllei0304@gmail.com>
2022-09-24 14:37:10 -0400

    REFACTOR: changes to repo pg

--- code_garden/views.py
+++ code_garden/views.py
@@ -1,4 +1,4 @@
-from flask import current_app, render_template, request
+from flask import current_app, redirect, render_template, request
 
 from code_garden.models import Repository
 
@@ -17,3 +17,11 @@ def index():
 def repo():
     repo_ = Repository(request.args.get("path"))
     return render_template("repo.html", repo_=repo_)
+
+
+@current_app.route("/checkout")
+def checkout():
+    repo_ = Repository(request.args.get("path"))
+    repo_.checkout(request.args.get("branch"))
+
+    return redirect(request.referrer)
