d65bbf9c148be39e57b2c41259caaccfa0979483
misterrager8 <chemllei0304@gmail.com>
2022-10-06 18:45:20 -0400

    REFACTOR: limit to 2 themes, refactor to be toggleable

--- code_garden/static/main.js
+++ code_garden/static/main.js
@@ -1,11 +1,20 @@
-$(document).ready(function() {
-    document.documentElement.setAttribute('data-theme', localStorage.getItem('CodeGarden'));
-});
+$(document).ready(function() {{
+    localStorage.getItem('CodeGarden') === 'dark' ? setDark() : setLight();
+}});
 
-function changeTheme(theme) {
-    localStorage.setItem('CodeGarden', theme);
+function setDark() {{
+    localStorage.setItem('CodeGarden', 'dark');
     document.documentElement.setAttribute('data-theme', localStorage.getItem('CodeGarden'));
-}
+    $('#dark').show();
+    $('#light').hide();
+}}
+
+function setLight() {{
+    localStorage.setItem('CodeGarden', 'light');
+    document.documentElement.setAttribute('data-theme', localStorage.getItem('CodeGarden'));
+    $('#light').show();
+    $('#dark').hide();
+}}
 
 function toggleDiv(divId) {
     $('#' + divId).fadeToggle(150);
